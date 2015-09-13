a0f5c51e8d699ae40ab667c2cceb2c1cdce1d084
Gareth Streeter <gareth.streeter@techex.co.uk>
2015-09-13 04:48:30 +0200

    renamed .js.inject to .inject.js

--- vidlytics/server/api/collection.js
+++ vidlytics/server/api/collection.js
@@ -122,7 +122,7 @@ Api.addRoute('video', {
 		});
 
 		var request = Meteor.http.get(url);
-		var injectThis = fs.readFileSync('../server/assets/app/injection-meta.js.inject', 'utf8');
+		var injectThis = fs.readFileSync('../server/assets/app/injection-meta.inject.js', 'utf8');
 		var injected = request.content.replace('<head>', '<head><base href="' + url + '" target="_blank"><script>' + injectThis + '</script>');
 		this.response.writeHead(200, {
 			'Set-Cookie': 'ident=' + ident + ';Path=/;'
